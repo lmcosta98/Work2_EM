6f27aef8750a942b6da398df836714c906055b47
Luís Miguel Costa <47531536+lmcosta98@users.noreply.github.com>
2022-12-29 20:03:09 +0000

    some cleanup and error correction

--- AC.R
+++ AC.R
@@ -12,21 +12,32 @@ library(factoextra)
 library(ggplot2)
 library(viridis)
 library(RColorBrewer)
+library(dendextend)
 
 # Load the datasets
-dados <-read.csv("/Users/luismiguel/Desktop/Uni/EM/2022/Work2_EM/datasets/Time_Happiness_GDP.csv")
+dados <-read.csv("/Users/luismiguel/Desktop/Uni/EM/2022/Work2_EM/datasets/Time_Happiness_GDP.csv") # dataset for only 2020
 data <- read.csv("/Users/luismiguel/Desktop/Uni/EM/2022/Work2_EM/datasets/Happiness.csv")
-n_dados <- read.csv("/Users/luismiguel/Desktop/Uni/EM/2022/Work2_EM/datasets/Happiness_GDP.csv")
+n_dados <- read.csv("/Users/luismiguel/Desktop/Uni/EM/2022/Work2_EM/datasets/Happiness_GDP.csv") # complete dataset for 10 years
 
 # Dataframes for clustering
+n_dados_clust <- n_dados[, -1]
+n_dados_clust
 tmp <- dados[, -1] # cluster based on time spent
 rownames(tmp) <- dados$Countries
-dados_clust <- tmp[, -c(1:2)]
-dados_viz <- tmp[, c(1:2)] # auxiliar for plotting
+dados_clust <- tmp[, -c(1:3)]
+dados_viz <- tmp[, c(1:3)] # auxiliar for plotting
 dados_viz_2 <- dados_viz # another auxiliar for plotting
 dados_hap <- tmp[, -2] # cluster based only on happiness
 dados_hap_gdp <- tmp[, c(1:2)]
 dados_hap_gdp
+
+
+# Setting color palette
+coul <- colorRampPalette(brewer.pal(3, "PuOr"))(50)
+
+myColors <- brewer.pal(5,"PuOr")
+names(myColors) <- levels(dat$grp)
+colScale <- scale_colour_manual(name = "grp",values = myColors)
 
 #### Clustering based on Happiness and GDP #####
 n_dados_clust <- n_dados[, -1]
@@ -37,7 +48,9 @@ kmeans_hap_gdp <- kmeans(n_dados_clust, 3, nstart = 20)
 kmeans_hap_gdp # By analyzing the clusters we can see that the countries tend to be
                # assigned to the same cluster on the several years. This is to be expected since 
                # GDP and happiness do not vary much in around 10 years.
-               # Can make use of this to cluster only on the happiness data from 2020.
+               # To make the results more legible we will cluster only on the happiness data from 2020.
+
+n_dados$cluster <- as.character(kmeans_hap_gdp$cluster)
 
 # Plot the silhouette graph
 d <- daisy(n_dados_clust)
@@ -51,12 +64,11 @@ resultadoIndices <- matrix(c(res$corrected.rand,res$avg.silwidth), byrow=TRUE,1,
 colnames(resultadoIndices) <- c("ARI","avg.Silhw")
 round(resultadoIndices, 3) # Small avg sil
 
-coul <- colorRampPalette(brewer.pal(11, "BrBG"))(50)
 heatmap(as.matrix(n_dados_clust), scale = "column", col = coul)
 #heatmap(as.matrix(tmp), scale = "column", col = coul)
 
 
-# clustering using only the 2020 data for happiness and gdp
+# Clustering using only the 2020 data for happiness and gdp
 nc <- NbClust(dados_hap_gdp, min.nc = 2, max.nc = 15, method = "kmeans") # Ideal number found is 3 clusters
 kmeans_hap_gdp_2020 <- kmeans(dados_hap_gdp, 3, nstart = 20)
 kmeans_hap_gdp_2020 # By analyzing the clusters we can see t
@@ -72,32 +84,34 @@ round(resultadoIndices, 3) # Small avg sil
 
 heatmap(as.matrix(dados_hap_gdp), scale = "column", col = coul)
 
+distances <-dist(dados_hap_gdp, method="euclidian") # Euclidean distance
+hc_complete = hclust(distances, method="complete") # Trying to obtain clusters that are more compact (data points more similiar to each)
+#hc_single = hclust(distances, method="single") # Trying to obtain fewer clusters
+dend <- as.dendrogram(hc_complete)
+dend <- dend %>% color_branches(k=4)
+plot(dend, main="Euclidean distance and furthest neighbour")
+#plot(hc_complete, hang=-1, main="Distancia euclidiana, e vizinho-mais-afastado")
+
 
 # Plot the clusters in relation to happiness and gdp
-n_dados$cluster <- kmeans_hap_gdp$cluster
-ggplot(n_dados, aes(x=GDP_Per_Capita, y=Happiness))+geom_point(aes(color=cluster))+scale_color_viridis(option = "H")
-
+dados_viz$cluster <- as.character(kmeans_hap_gdp_2020$cluster)
+dados_viz
+ggplot(dados_viz, aes(x=log(GDP_Per_Capita), y=Happiness, colour=cluster))+geom_point()
+#plot(log(dados_viz$GDP_Per_Capita), dados_viz$Happiness, col=dados_viz$cluster)
+ggplot(dados_viz, aes(x=log(GDP_Per_Capita), y=Happiness, colour=Continent))+geom_point()
 # Plotting frequency of happiness responses and comparing total vs cluster happiness
-hist(n_dados$Happiness, xlab = "Happiness", main="Happiness on complete dataset")
+
 
 #### Need to add the lines for the total frequency
-data_clust_1 <- n_dados[n_dados$cluster == 1,]
-hist(data_clust_1$Happiness, xlab = "Happiness", main="Happiness on cluster 1")
-#hist(data_clust_1$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 1")
-
-data_clust_2 <- n_dados[n_dados$cluster == 2,]
-hist(data_clust_2$Happiness, xlab = "Happiness", main="Happiness on cluster 2")
-#hist(data_clust_2$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 2")
-
-data_clust_3 <- n_dados[n_dados$cluster == 3,]
-hist(data_clust_3$Happiness, xlab = "Happiness", main="Happiness on cluster 3")
-#hist(data_clust_3$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 3")
-
-# checking countries per cluster
-dados_hap_gdp$cluster <- kmeans_hap_gdp_2020$cluster
-dados_hap_gdp[dados_hap_gdp$cluster == 1,] # Countries in cluster 1
-dados_hap_gdp[dados_hap_gdp$cluster == 2,] # Countries in cluster 2
-dados_hap_gdp[dados_hap_gdp$cluster == 3,] # Countries in cluster 3
+# plotting the distrution of the answers regarding happines per country
+data_clust_1 <- n_dados[n_dados$cluster == '1',]
+data_clust_2 <- n_dados[n_dados$cluster == '2',]
+data_clust_3 <- n_dados[n_dados$cluster == '3',]
+par(mfrow=c(2,2))
+hist(n_dados$Happiness, xlab = "Happiness", main="Happiness on complete dataset")
+hist(data_clust_1$Happiness, xlab = "Happiness", main="Happiness on cluster 1", col="red")
+hist(data_clust_2$Happiness, xlab = "Happiness", main="Happiness on cluster 2", col="green")
+hist(data_clust_3$Happiness, xlab = "Happiness", main="Happiness on cluster 3", col="blue")
 
 
 
@@ -112,7 +126,7 @@ kmeans_clust <- kmeans(dados_clust, 4, nstart = 20)
 kmeans_clust
 
 # Adding the clusters to the data to be visualized
-dados_viz$cluster <- kmeans_clust$cluster
+dados_viz$cluster <- as.character(kmeans_clust$cluster)
 dados_viz
 
 # Plot the silhouette graph
@@ -129,9 +143,9 @@ colnames(resultadoIndices) <- c("ARI","avg.Silhw")
 round(resultadoIndices, 3) # Small avg silhouette and a positive ARI, everything seems to be in order
 
 # Plotting the clusters on the data relative to Happiness and GDP
-ggplot(dados_viz, aes(x=GDP_Per_Capita, y=Happiness))+geom_point(aes(color=cluster))+scale_color_viridis(option = "A")
-# trying switching the axis
-ggplot(dados_viz, aes(x=Happiness, y=GDP_Per_Capita))+geom_point(aes(color=cluster))+scale_color_viridis(option = "A")
+ggplot(dados_viz, aes(x=GDP_Per_Capita, y=Happiness))+geom_point(aes(color=cluster)) # It does not seem to be a connection
+                                                                                # between the cluster formed by time spent 
+                                                                                # and the happiness/gdp relationship
 
 # Creating a dendrogram to plot the clusters
 distances <-dist(dados_clust, method="euclidian") # Euclidean distance
@@ -144,6 +158,17 @@ plot(hc_single, hang=-1, main="Distancia euclidiana, e vizinho-mais-proximo")
 # Creating a heatmap to see relationships between countries and variables
 heatmap(as.matrix(dados_clust), scale = "column", col = coul)
 #heatmap(as.matrix(dados_clust), scale = "column", col = coul)
+
+
+#### Clustering the variables of the time spent dataset
+cols.cor <- cor(dados_clust, use = "pairwise.complete.obs", method = "pearson") # using pearson correlation for clustering variables
+dist <- as.dist(1 - cols.cor)
+hc_complete = hclust(dist, method="complete")
+dend <- as.dendrogram(hc_complete)
+dend <- dend %>% color_branches(k=4)
+plot(dend, main="Pearson Correlation and nearest neighbour")
+dev.off()
+
 
 
 
@@ -177,34 +202,4 @@ ggplot(dados_viz_2, aes(x=GDP_Per_Capita, y=Happiness))+geom_point(aes(color=clu
 
 heatmap(as.matrix(dados_hap), scale = "column", col = coul)
 
-# No significant difference found when including happiness, 
-# so we will use the results from the first clustering.
-
-
-# Analyzing the clusters
-dados$cluster <- kmeans_clust$cluster
-data <- merge(data, dados, by="Countries")
-#data <- data.frame(data)
-data
-
-# The same as above, can be removed
-# Plotting frequency of happiness responses and comparing total vs cluster happiness
-hist(data$Happiness.x, xlab = "Happiness", main="Happiness on complete dataset")
-
-#### Need to add the lines for the total frequency
-data_clust_1 <- data[data$cluster == 1,]
-hist(data_clust_1$Happiness.x, xlab = "Happiness", main="Happiness on cluster 1")
-#hist(data_clust_1$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 1")
-
-data_clust_2 <- data[data$cluster == 2,]
-hist(data_clust_2$Happiness.x, xlab = "Happiness", main="Happiness on cluster 2")
-#hist(data_clust_2$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 2")
-
-data_clust_3 <- data[data$cluster == 3,]
-hist(data_clust_3$Happiness.x, xlab = "Happiness", main="Happiness on cluster 3")
-#hist(data_clust_3$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 3")
-
-data_clust_4 <- data[data$cluster == 4,]
-hist(data_clust_4$Happiness.x, xlab = "Happiness", main="Happiness on cluster 4")
-#hist(data_clust_4$GDP_Per_Capita, xlab = "GDP per Capita", main="GDP on cluster 4")
-
+# No significant difference found when including happiness.
