e9e94e62919569bcc5cd073f219f187bed956a9c
FredVieira16 <84329028+FredVieira16@users.noreply.github.com>
2022-12-31 15:05:22 +0000

    Update

--- Pca.R
+++ Pca.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(NbClust)
 #library(ggfortify)
 d<-read_excel("Time_Use.xlsx")
+par(mfrow = c(1,1))
 #View(d)
 names<-d[1]
 dadosx<-d[,-1]
@@ -22,13 +23,27 @@ summa=summary(pcas)
 
 variance = pcas$sdev^2 / sum(pcas$sdev^2)
 print(variance)
-qplot(c(1:14), variance) +
-  geom_line() +
-  geom_point(size=2)+
-  xlab("Principal Component") +
-  ylab("Variance Explained") +
-  ggtitle("Find Best Number of PC's") +
-  ylim(0, 0.30)
+plot(variance, type="o", col="blue", xlab=paste("Number of PC's"),ylab=paste("Variance Explained (%)"))
+title(main="Variance Explained Vs Number PC's", col.main="Black", font.main=4)
+
+############ Variance Explained Vs Number PC's ################
+
+variance_index<-c(2,5,8,11,14,17,20,23,26,29,32,35,38,41)
+variance_eva <- c(0)
+#print(variance_eva)
+#print(variance_eva[length(variance_eva)])
+for (x in variance_index){
+  variance_eva<-append(variance_eva,variance_eva[length(variance_eva)]+round(summary(pcas)$importance[x]*100))
+}
+
+variance_eva=setdiff(variance_eva,0)
+print(variance_eva)
+
+plot(variance_eva, type="o", col="blue", xlab=paste("Number of PC's"),ylab=paste("Variance Explained (%)"))
+
+# Create a title with a red, bold/italic font
+title(main="Variance Explained Vs Number PC's", col.main="Black", font.main=4)
+
 ###############
 # biplot preservando a metrica das colunas
 pcas.data<-data.frame(PC1=pcas$x[,1],
@@ -36,7 +51,7 @@ pcas.data<-data.frame(PC1=pcas$x[,1],
                       PC3=pcas$x[,3],
                       PC4=pcas$x[,4],
                       PC5=pcas$x[,5])
-plot(pcas.data,pch=5,col=c("blue","red","green","brown","pink")) # Check all 5 dim
+plot(pcas.data,pch=5) # Check all 5 dim
 
 #Compare only 2 of them
 biplot(pcas,choices=1:2,pch=15,col=c("blue","red"), cex=0.8,cex.axis=0.7,arrow.len = 0.05,xlab=paste(" PC1  (", (round(100*summa$importance[2,1],digits=1)), " % )"),
@@ -56,22 +71,7 @@ biplot(pcas,choices=1:2,pch=15,col=c("blue","red"), cex=0.8,cex.axis=0.7,arrow.l
 # 3(Belgium),13(Italy), 9(Germany),7(Finland),29(UK),20(Netherlands), are similar to each other.
 # 8(France), 11(Hungary),10(Greece) are also another group.
 
-############ Variance Explained Vs Number PC's ################
 
-variance_index<-c(2,5,8,11,14,17,20,23,26,29,32,35,38,41)
-variance_eva <- c(0)
-#print(variance_eva)
-#print(variance_eva[length(variance_eva)])
-for (x in variance_index){
-    variance_eva<-append(variance_eva,variance_eva[length(variance_eva)]+round(summary(pcas)$importance[x]*100))
-}
-variance_eva=setdiff(variance_eva,0)
-print(variance_eva)
-
-plot(variance_eva, type="o", col="blue", xlab=paste("Number of PC's"),ylab=paste("Variance Explained (%)"))
-
-# Create a title with a red, bold/italic font
-title(main="Variance Explained Vs Number PC's", col.main="Black", font.main=4)
 
 
 ############### Cluster ################
@@ -84,30 +84,52 @@ dados2G
 #Visualizar os cluster da k-medias:
 str(dados2G)
 a=prcomp(dadosx, scale=TRUE)
-nc <- NbClust(a$x,min.nc=3,max.nc=15,method="kmeans")
-plot(a$x[,1:5], col = dados2G$cluster, pch=19, main="kmeans, 5 grupos")
+#nc <- NbClust(a$x,min.nc=3,max.nc=15,method="kmeans")
+print(dados2G$cluster)
+colors <- c()
+for (z in dados2G$cluster){
+  if (z==1){
+    colors<-append(colors,'#00FF00')
+  }
+  if (z==2){
+    colors<-append(colors,'#446455')
+  }
+  if (z==3){
+    colors<-append(colors,'#FDD262')
+  }
+  if (z==4){
+    colors<-append(colors,'#D3DDDC')
+  }
+  if (z==5){
+    colors<-append(colors,'#000000')
+  }
+}
+print(colors)
+#  ("#00FF00","#446455","#FDD262", "#D3DDDC","#000000")
+plot(a$x[,1:5], col = dados2G$cluster,pch=19, main="kmeans, 5 grupos")
 #install.packages("cluster")
 library(cluster)
 D <- daisy(dadosx)
 sil_cl <- silhouette(dados2G$cluster, D)
 rownames(sil_cl) <- names$Country
-plot(sil_cl, col = c("blue", "purple","orange","red","black"),cex.names = par("cex.axis"))
+plot(sil_cl, col = c("#00FF00","#446455","#FDD262", "#D3DDDC","#000000"),cex.names = par("cex.axis"))
 
 
 #HEATMAP
 library(RColorBrewer)
-coul <- colorRampPalette(brewer.pal(11, "BrBG"))(50)
+coul <- colorRampPalette(brewer.pal(11, "PuOr"))(50)
 
-cen <- a$x
+cen <- a$x[,1:5]
 rownames(cen) <- names$Country
-colnames(cen) <- c("F1", "F2", "F3", "F4")
-heatmap(cen, scale = "column", col = coul, par = 1)
+colnames(cen) <- c("PC1", "PC2", "PC3", "PC4","PC5")
+heatmap(cen, scale = "column", col = coul)
 abline(h = 0.685, col = "red", lwd = 2, lty = 2)
 abline(h = 0.60, col = "red", lwd = 2, lty = 2)
 legend(
   0, 1,
-  ncol = 3, legend = c("-4", "-3", "-2", "-1", "0", "1", "2", "3", "4"),
-  fill = colorRampPalette(brewer.pal(11, "BrBG"))(9)
+  ncol = 3,
+  legend = c("-100%", "-75%", "-50%", "-25%", "0%", "25%", "50%", "75%", "100%"),
+  fill = colorRampPalette(brewer.pal(9, "PuOr"))(9)
 )
 
 #autoplot(, x = 1, y = 2, colour = "Species",
